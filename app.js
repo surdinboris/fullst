0873b005c0cdb8a07511f939794c5e9b7ef19304
borrik <borrik@live.ru>
2019-02-24 16:09:47 +0200

    added web functionality - listing and html display

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ console.log('app initialized')
 
 $(function () {
     'use strict';
-
+    console.log('app init')
+    // this.$el.append(_.template('I am <%= amount %> dumb')({amount:200}))
     // kick things off by creating the `App`
- new app.AppView();
+ // new app.AppView();
 });
