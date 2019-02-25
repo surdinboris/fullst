3a1c8264aaae21765d17e62cb9d889c237681ea9
borrik <borrik@live.ru>
2019-02-25 16:29:31 +0200

    added client-rendered content page with json data for initial fullfilment

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 
-var app = app || {};
-
 $(function () {
     'use strict';
     console.log('app init');
     console.log(filelist);
     let files=$("#files")[0];
-    console.log(files)
+    console.log(files);
     for(let i=0; i<filelist.length; i++) {
         let newlink = document.createElement('a');
         console.log(newlink);
         newlink.setAttribute('class', 'filename');
-        newlink.appendChild(document.createTextNode(filelist[i]));
+        let linkname=filelist[i].split("\\");
+        console.log('ppp',linkname);
+        newlink.appendChild(document.createTextNode(linkname[linkname.length-1]));
         newlink.setAttribute('href', filelist[i]);
-        files.appendChild(newlink)
+        files.appendChild(newlink);
         files.appendChild(document.createElement('br'))
     }
-    console.log(files.childNodes)
-    // let tmpl = _.template($('#item-template').html())
-    // tmpl({completed:true})
-    // this.$el.append(_.template('I am <%= amount %> dumb')({amount:200}))
-    // kick things off by creating the `App`
- // new app.AppView();
 });
