937ffbf7f44c9d6cb22165412a59eeaba89a2846
borrik <borrik@live.ru>
2019-02-25 11:25:24 +0200

    passin to native nodejs - starting to build front for files

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 
 var app = app || {};
-console.log('app initialized');
-let tmpl = _.template($('#item-template').html())
-tmpl({completed:true})
 
 $(function () {
     'use strict';
-    console.log('app init')
-    let tmpl = _.template($('#item-template').html())
+    console.log('app init');
+    console.log(filelist)
+    // let tmpl = _.template($('#item-template').html())
     // tmpl({completed:true})
     // this.$el.append(_.template('I am <%= amount %> dumb')({amount:200}))
     // kick things off by creating the `App`
--- nodeservr.js
+++ nodeservr.js
@@ -1,81 +1,54 @@
-var express = require('express');
-var app = express();
 const {createReadStream} = require("fs");
-const {stat, readdir} = require("fs").promises;
+const {stat, readdir,readFile} = require("fs").promises;
 const {parse} = require("url");
 const {join,resolve, sep, extname} = require("path");
 const baseDirectory = process.cwd();
+const mime = require('mime');
+let http =require('http');
 
-app.set("view options", {layout: false});
-app.engine('html', require('ejs').renderFile);
-
-function toFSpath(url) {
-    let {pathname} = parse(url);
-    let filepath = resolve(decodeURIComponent(pathname).slice(1));
-
-    if (filepath != baseDirectory &&
-        !filepath.startsWith(baseDirectory + sep)) {
-        throw {status: 403, body: "Forbidden"};
-    }
-    return filepath;
-
-}
-
-app.get('/welcome', function (req, res) {
-    console.log('welcome')
-    res.send('<b>Hello</b> welcome to my http server made with express');
-});
-
-app.get('*', async function (request, res) {
+let server=http.createServer(async function (request,response) {
     let filepath = toFSpath(request.url);
     // console.log('toFSpath',request.url,toFSpath(request.url))
     let stats;
     let isdir;
+
     try {
         stats = await stat(filepath);
         isdir=await stats.isDirectory()
     } catch (error) {
         //console.log(error);
-        if (error.code == "ENOENT") throw error;
-        // else res.send("File not found");
+        if (error.code == "ENOENT") {
+            response.writeHead(404, `resource not found ${error}`);
+            response.end();
+            console.log(error.toString().replace("\n"));
+            return
+        }
     }
     if (isdir) {
         //fullpath to enter to  subsub levels
-        let urllist = await readdir(filepath);
-        // let urllist = (await readdir(path)).map((c) => {
-        //     //patch - avoid unnecessary '/' addidtions
-        //     if (request.url == "/") {
-        //         res.send( `<a href=${c}>${c}</a><br>`);
-        //     }
-        //      res.send( `<a href=${request.url + "/" + c}>${c}</a><br>`)
-        // });
-        let resp=urllist.map((u)=>{
-            if (request.url == "/") {
-                return (`<a href=${u}>${u}</a><br>`);
-            }
-                 return( `<a href=${request.url + "/" + u}>${u}</a><br>`)
-             });
-        resp=resp.join("\n");
-        res.send(resp)
+        let filelist = await readdir(filepath);
+        let filefront= await readFile('fileview.html');
+        let json=JSON.stringify({"filelist":filelist});
+        response.write(filefront);
+        response.end(`<script type="text/javascript">let filelist= ${json}</script>`)
     }
-
     else {
-        // console.log(extname(filepath),filepath);
-        if (extname(filepath) == ".html"){
-            res.render(filepath);
-        }
-        else{
-            let readstream=createReadStream(filepath);
-            readstream.pipe(res)}
+        let mimeType=await mime.getType(filepath);
+        console.log('getType',filepath,`{Content-Type:${mimeType}}`);
+        response.setHeader("Content-Type",mimeType);
+        response.end(await readFile(filepath))
     }
 });
 
-//
 
+function toFSpath(url) {
+    let {pathname} = parse(url);
+    let filepath = resolve(decodeURIComponent(pathname).slice(1));
+    if (filepath != baseDirectory &&
+        !filepath.startsWith(baseDirectory + sep)) {
+        throw {status: 403, body: "Forbidden"};
+    }
+    return filepath;
+}
 
-// app.use(function(req, res, next) {
-//     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-// });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
-});
+server.listen(3000);
