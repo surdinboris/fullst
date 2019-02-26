e260a80fc08c5e92bb3f146b62a3f12443e6f7c3
boris surdin <borissurdin@2FD34AF3DE22B1.local>
2019-02-26 22:29:21 +0200

    added stats coleection - to be implemented in front end

--- app.js
+++ app.js
@@ -4,25 +4,21 @@ $(function () {
     console.log('app init');
     console.log(filelist);
     let files=$("#files")[0];
-    for(let key in filelist) {
-        let newlink = document.createElement("div");
-        newlink.setAttribute('class', 'filerow');
-        // newlink.appendChild(document.createTextNode(key));
-        for(let fkey in filelist[key]){
-            let att = document.createElement('b');
-            if(fkey=='fullname') {
-                att.setAttribute('href', filelist[key][fkey]);
-                att = document.createElement('a');
-            }
-            att.setAttribute('class', fkey);
-            att.appendChild(document.createTextNode(filelist[key][fkey]));
-            if(fkey=='fullname') {
-                att.setAttribute('href', filelist[key][fkey]);
-            }
-            newlink.appendChild(att)
-        }
 
-        files.appendChild(newlink);
-        files.appendChild(document.createElement('br'))
+    for(let key in filelist) {
+        for(let fkey in filelist[key]){
+            let controw = document.createElement('div');
+            controw.setAttribute('class', 'grid-item');
+            if(fkey=='fullname') {
+                let aelem = document.createElement('a');
+                aelem.setAttribute('href', filelist[key][fkey]);
+                aelem.appendChild(document.createTextNode(filelist[key][fkey]))
+                controw.appendChild(aelem)
+            }
+            else {
+                controw.appendChild(document.createTextNode(filelist[key][fkey]));
+                }
+            files.appendChild(controw)
+        }
     }
 });
