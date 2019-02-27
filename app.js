e005e3034c4750aae610b1c37e9b0e2688722fc6
boris surdin <borissurdin@2FD34AF3DE22B1.local>
2019-02-27 21:21:35 +0200

    added coloring. some attrs dictonary need to be generated to make looping via array of keys for further filtering

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-
 $(function () {
     'use strict';
     console.log('app init');
     console.log(filelist);
-    let files=$("#files")[0];
+    let files = $("#files")[0];
+    for (let fileobj in filelist) {
+        //subsequence for "non columns" attributes
+            let isdir=filelist[fileobj]["_isdir"];
 
-    for(let fileobj in filelist){
-        for(let fkey in filelist[fileobj]){
+        for (let fkey in filelist[fileobj]) {
+            if(fkey[0]=='_'){
+                continue
+            }
+
             let contentrow = document.createElement('div');
             contentrow.setAttribute('class', 'grid-item');
-
-            if(fileobj == 'headers'){}
-
-            else{
-                if(fkey=='fullname') {
+            if (fileobj == 'headers') {
+                contentrow.appendChild(document.createTextNode(filelist[fileobj][fkey]));
+            } else {
+                if (fkey == 'fullname') {
                     let aelem = document.createElement('a');
-
-
                     aelem.setAttribute('href', filelist[fileobj][fkey]);
-
                     aelem.appendChild(document.createTextNode(fileobj));
                     contentrow.appendChild(aelem)
-                }
-                else {
-                    contentrow.appendChild(document.createTextNode(filelist[fileobj][fkey]));
-                }}
+                    //coloring depending by dir or file
+                    if(isdir){
+                        aelem.setAttribute('class', 'isdir');
+                    }
+                    else{
+                        aelem.setAttribute('class', 'isfile');
+                    }
 
-            files.appendChild(contentrow)
+                } else {
+
+                        contentrow.appendChild(document.createTextNode(filelist[fileobj][fkey]));
+
+                }
+            }
+            files.appendChild(contentrow);
+
+
         }
+
     }
 });
