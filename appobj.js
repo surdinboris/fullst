c91d3f7c3cc4bbe2b9820592d6203b954be08219
borrik <borrik@live.ru>
2019-03-06 16:30:38 +0200

    more detailed html generating with class\data from json object

--- appobj.js
+++ appobj.js
@@ -7,20 +7,19 @@ $(function () {
     //building file object prototype
     //and fulfill it from json data with additional event binding
     //and computed parameters such parent object
-    function DirRecord(params){
+    function DirRecord(headers,opts){
         //looping via data object and assigning standard attributes
         this.populheaders.forEach(function (header) {
-            this[header]=params[header]
+            this[header]=headers[header]
         },this);
         //computation and appending calculated attributes
-        this.fdname=params.fdname;
-        this._isdir=params._isdir;
-        this.parenturl = params.parenturl;
-        this.filtered=params.filter;
-        if(params.fdtype == 'dir'){
+        this.fdname=opts.fdname;
+        this.parenturl = opts.parenturl;
+        this.filtered=opts.filter;
+        if(headers._isdir == true){
             this.icon="/images/directory.svg"
         }
-        if(params.fdtype == 'file'){
+        if(headers.fdtype == false){
             this.icon="/images/file.svg"
         }
     }
@@ -32,7 +31,7 @@ $(function () {
         let headers=data.headers;
         delete data.headers;
         for(let df in data){
-            let dirrec = new DirRecord(data[df]);
+            let dirrec = new DirRecord(data[df],{filtered:false});
             dirpage.push(dirrec);
         }
         return dirpage
