b7555b7a644f1f42376c540642373c4b14c58ff4
borrik <borrik@live.ru>
2019-03-06 16:15:22 +0200

    more detailed html generating with class\data from json object

--- appobj.js
+++ appobj.js
@@ -2,7 +2,7 @@ $(function () {
     'use strict';
     console.log('app init');
     let files = $("#files")[0];
-    console.log(filelist);
+    //console.log(filelist);
 
     //building file object prototype
     //and fulfill it from json data with additional event binding
@@ -17,7 +17,6 @@ $(function () {
         this._isdir=params._isdir;
         this.parenturl = params.parenturl;
         this.filtered=params.filter;
-
         if(params.fdtype == 'dir'){
             this.icon="/images/directory.svg"
         }
@@ -28,22 +27,19 @@ $(function () {
     //standard attributes interface definition for looping via data object
     DirRecord.prototype.populheaders = ['fullname','fsize','mtime', 'fsize','_isdir'];
 
-
-    //test
-    //     let newfile = new File({fdname:'test.txt', filter:true, parenturl:'http://nextra42/'});
-    //
-    //     console.log(newfile.getfullurl());
-
-    (function objgen(data){
+    function objgen(data){
         let dirpage=[];
+        let headers=data.headers;
+        delete data.headers;
         for(let df in data){
-            console.log(df)
-            let dirrec = new DirRecord(df);
-            console.log(dirrec)
-
+            let dirrec = new DirRecord(data[df]);
+            dirpage.push(dirrec);
         }
+        return dirpage
+    }
 
-    })(filelist);
+    let dirpage=objgen(filelist);
+    console.log(dirpage);
 
     function render(filelist) {
         files.innerHTML = '';
