519931eda33c9ff9b82efacb19a05b0a6df1d66f
borrik <borrik@live.ru>
2019-03-07 08:47:55 +0200

    more detailed html generating with class\data from json object

--- appobj.js
+++ appobj.js
@@ -3,35 +3,42 @@ $(function () {
     console.log('app init');
     let files = $("#files")[0];
     //console.log(filelist);
-
     //building file object prototype
-    //and fulfill it from json data with additional event binding
-    //and computed parameters such parent object
     function DirRecord(headers,opts){
         //looping via data object and assigning standard attributes
         this.populheaders.forEach(function (header) {
             this[header]=headers[header]
         },this);
+
+        //adding optional passed attrs
+        this.filtered=opts.filtered;
+
         //computation and appending calculated attributes
-        this.fdname=opts.fdname;
-        this.parenturl = opts.parenturl;
-        this.filtered=opts.filter;
         if(headers._isdir == true){
             this.icon="/images/directory.svg"
         }
         if(headers.fdtype == false){
             this.icon="/images/file.svg"
         }
+        let splitted = this.fullname.split(/(?=\\)/g);
+        console.log(splitted);
+        this.fdname=splitted[splitted.length-1];
+        splitted.pop();
+        this.parenturl = splitted.join("")
+        //changing filesize untits
+        if(opts.sizeunit == 'Kb'){
+            this.fsize = Number(this.fsize)/1024
+        }
     }
     //standard attributes interface definition for looping via data object
-    DirRecord.prototype.populheaders = ['fullname','fsize','mtime', 'fsize','_isdir'];
+    DirRecord.prototype.populheaders = ['fullname','fsize','mtime','_isdir'];
 
     function objgen(data){
         let dirpage=[];
         let headers=data.headers;
         delete data.headers;
         for(let df in data){
-            let dirrec = new DirRecord(data[df],{filtered:false});
+            let dirrec = new DirRecord(data[df],{filtered:false, sizeunit:'Kb});
             dirpage.push(dirrec);
         }
         return dirpage
